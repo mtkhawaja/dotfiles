798f840c105fee5d176ec4dc3c36384c6ed9844d
Muneeb Khawaja <36654508+mtkhawaja@users.noreply.github.com>
2021-04-12 04:17:47 -0400

    Updated variable names for consistency.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-function install_package() {
+function install_application() {
     application=$1
     if ! [ -x "$(command -v "$application")" ]; then
         apt install "$application" -y
     fi
 }
 
-package_list="${BASH_SOURCE%/*}/data/application_list.txt"
+application_list="${BASH_SOURCE%/*}/data/application_list.txt"
 while IFS= read -r line; do
-    echo "$line"
     install_package "$line"
-done <"$package_list"
+done <"$application_list"
