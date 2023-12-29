4889169e511df1437aeda32eb34635ea968608b7
Muneeb Khawaja <36654508+mtkhawaja@users.noreply.github.com>
2023-12-28 19:59:47 -0500

    Updated permissions to make './utility/commit_as_executable.sh' executable

---
+++ bin/.local/scripts/utility/commit_as_executable.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# See: https://stackoverflow.com/questions/21691202/how-to-create-file-execute-mode-permissions-in-git-on-windows
+
+TARGET_FILE=$1
+
+if ! [ -f "$TARGET_FILE" ]; then
+  echo "File does not exist: '$TARGET_FILE'"
+  exit 1
+fi
+
+echo "Attempting to make '$TARGET_FILE' executable"
+git add --chmod=+x -- "$TARGET_FILE"
+git commit -m "Updated permissions to make '$TARGET_FILE' executable"
